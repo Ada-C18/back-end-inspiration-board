a0b7c6707319b73c69358896f05ace860cb265e0
Margaret <y.margaret.lu+github@gmail.com>
2022-12-22 13:53:20 -0800

    did initial setup up, no blueprints

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,6 @@
 from app import db
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
