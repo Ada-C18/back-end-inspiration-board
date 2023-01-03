df63db082503d84af235f8b430869b91e4216e83
Tasha <tashachang2@gmail.com>
2023-01-03 12:03:14 -0800

    added helper method to validate object id and get one board endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.board import Board
 
@@ -11,7 +11,7 @@ def get_all_boards():
     result = []
     for item in boards:
         result.append(item.to_dict())
-    
+
     return jsonify(result)
 
 @board_bp.route("", methods=["POST"])
@@ -23,3 +23,25 @@ def add_board():
     db.session.commit()
 
     return {"message": f"Successfully created new board with id = {new_board.board_id}"}, 201
+
+@board_bp.route("/<board_id>", methods=["GET"])
+def get_one_board(board_id):
+    chosen_board = get_one_obj_or_abort(Board, board_id)
+
+    board_dict = chosen_board.to_dict()
+
+    return jsonify(board_dict), 200
+
+def get_one_obj_or_abort(cls, obj_id):
+    try:
+        obj_id = int(obj_id)
+    except ValueError:
+        response_str = f"Invalid ID: `{obj_id}`. ID must be an integer"
+        abort(make_response(jsonify({"message":response_str}), 400))
+
+    matching_obj = cls.query.get(obj_id)
+
+    if not matching_obj:
+        return abort(make_response({"message": f"{cls.__name__} {obj_id} not found"}, 404))
+
+    return matching_obj
