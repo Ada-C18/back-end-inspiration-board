fc1f4603641a54f2b82615745669bc4b6a2187ba
Justina <justinakliu@gmail.com>
2023-01-03 13:24:24 -0500

    migrations added

--- app/models/board.py
+++ app/models/board.py
@@ -3,7 +3,7 @@ from app import db
 class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
-    owner = db.String(db.String) 
+    owner = db.Column(db.String) 
 
     cards = db.relationship("Card", back_populates="board", lazy=True)
 
