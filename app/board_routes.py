52bc6688ba2e2d56d644630da076836b67d9b5d6
Isabella Van Stedum <isabella.vanstedum@gmail.com>
2023-01-03 13:15:05 -0600

    did migration for board model

--- app/board_routes.py
+++ app/board_routes.py
@@ -3,10 +3,10 @@ from app import db
 from app.models.board import Board
 # example_bp = Blueprint('example_bp', __name__)
 
-board_bp = Blueprint('board_bp', __name__, url_prefix="/boards")
+board_bp = Blueprint('boards', __name__, url_prefix="/boards")
 
 #Get Routes
-@board_bp("", methods=['GET'])
+@board_bp.route("", methods = ['GET'])
 def get_all_boards():
 
     boards = Board.query.all()
