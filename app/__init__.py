f5d5caf7ea1dd5ed01c3601a35e98a54656df921
Annie <annie.gallagher7@gmail.com>
2023-01-03 12:20:49 -0700

    adds routes folder and registers blueprints in app

--- app/__init__.py
+++ app/__init__.py
@@ -14,21 +14,21 @@ def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-        "SQLALCHEMY_DATABASE_URI")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
     # Import models here for Alembic setup
     from app.models.board import Board
     from app.models.card import Card
 
-
-
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    # from .routes import example_bp
-    # app.register_blueprint(example_bp)
+    from .routes import board
+    from .routes import card
+
+    app.register_blueprint(board.bp)
+    app.register_blueprint(card.bp)
 
     CORS(app)
     return app
--- app/models/card.py
+++ app/models/card.py
@@ -1,6 +1,7 @@
 from app import db
 
+
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True)
-    message = db.Column(db.String)
+    message = db.Column(db.String(40))
     likes_count = db.Column(db.String)
