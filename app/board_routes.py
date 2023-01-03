757d89e71eae6c63b04da1122936282ffe453af5
Yael <yaelechols@gmail.com>
2023-01-03 11:05:53 -0800

    Adds model import to head of board_routes

--- app/board_routes.py
+++ app/board_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
