915a5612e0ac419762c4f0f63c21caa92f58830e
francescafr <hello@francesca.fyi>
2023-01-03 08:18:18 -0800

    created initial database, Card and Board models, routes under development

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,8 @@
 from app import db
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    body = db.Column(db.String)
+    liked = db.Column(db.Boolean)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
+    board = db.relationship("Board", back_populates="cards")
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,199 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint
+from flask import abort, make_response # for error handling / represents current HTTP response
+from flask import jsonify # for return formatting
+from flask import request # represents current HTTP request
 from app import db
+from app.models.board import Board
+from app.models.card import Card
 
-# example_bp = Blueprint('example_bp', __name__)
+# What routes are needed?
+    # want to be able to GET: board + all its cards
+        # /boards/<board_id>/cards
+    # want to be able to POST: new board, new cards (attached to board)
+        # /boards(new board)
+        # /cards (new card)
+    # want to be able to change (PATCH): board title, card title, description, liked status
+        # /boards/<board_id>
+        # /cards/<card_id>
+
+    # want to be able to delete (DELETE): board - and all its cards, individual cards
+        # /boards/<board_id>
+        # /cards/<card_id>
+
+
+# what data structures are needed?
+    # Two tables: Boards and Cards
+        # Cards: id, body, liked, board_id
+        # Boards: id, title
+
+# what data relationship is needed?
+    # One-to-many (one board has many cards)
+
+# Need to perform error handling (with validate function for board and card id)
+
+cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
+boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+
+# CREATE A NEW CARD
+@cards_bp.route("", methods=["POST"])
+def create_card():
+    request_body = request.get_json()
+    new_card = Card(
+        body=request_body["body"],
+        liked=False,
+        board_id=request_body["board_id"])
+
+    db.session.add(new_card)
+    db.session.commit()
+
+    return make_response(f"Card {new_card.body} created")
+
+# GET ALL CARDS 
+@cards_bp.route("", methods=["GET"])
+def get_cards():
+    all_cards = Card.query.all()
+    card_list =[]
+    for card in all_cards:
+        card_list.append({
+            "card_id":card.card_id,
+            "body":card.body,
+            "liked": card.liked,
+            "board_id": card.board_id
+        })
+    return jsonify(card_list)
+
+# GET CARD BY ID
+@cards_bp.route("/<card_id>", methods=["GET"])
+def get_card(card_id):
+    card_id = int(card_id) 
+    card = Card.query.get(card_id)
+    return ({
+        "card_id": card.card_id, 
+        "body" : card.body,
+        "liked": card.liked,
+        "board_id": card.board_id})
+
+# UPDATE CARD - LIKED STATUS, ETC
+
+@cards_bp.route("/<card_id>", methods=["PATCH"])
+def update_card(card_id):
+    card_id = int(card_id)
+# -----------------------------------------------------------------
+
+# CREATE A NEW BOARD
+@boards_bp.route("", methods=["POST"])
+def create_board():
+    request_body = request.get_json() # retrieve body of HTTP request
+
+    #create Board class object from request_body
+    new_board = Board(title=request_body["title"])
+
+    # add to database
+    db.session.add(new_board)
+    db.session.commit()
+
+    return make_response(f"board '{new_board.title}' created")
+
+# GET LIST OF ALL BOARDS
+@boards_bp.route("", methods=["GET"])
+def get_boards():
+    # retrieve all boards from database
+    all_boards = Board.query.all()
+    board_list = []
+    for board in all_boards:
+        board_list.append({
+            "board_id" : board.board_id,
+            "title": board.title,
+        })
+    return jsonify(board_list)
+
+# GET BOARD INFO + ALL CARDS ASSOCIATED WITH BOARD
+@boards_bp.route("/<board_id>/cards",methods=["GET"])
+def get_board(board_id):
+    #retrieve specific board from database
+    board = Board.query.get(board_id)
+    # create dictionary for return content
+    board_dict = {}
+    # create list for card content
+    card_list = []
+    # build card list
+    for card in board.cards:
+        card_dict = {
+            "card_id": card.card_id,
+            "body": card.body,
+            "liked": card.liked
+        }
+        card_list.append(card_dict)
+    board_dict["board_id"] = board.board_id
+    board_dict["title"] = board.title
+    board_dict["cards"] = card_list
+
+    return board_dict
+
+
+
+
+# ------------------ Practice Database and routes without postgres
+
+
+# cards = [
+#     {"id": 1, "body" : "Mexico", "liked": False, "board_id": 1},
+#     {"id": 2, "body" : "Canada", "liked": False, "board_id": 1},
+#     {"id": 3, "body" : "shopping", "liked": False, "board_id": 2},
+#     {"id": 4, "body" : "Animal Farm", "liked": False, "board_id": 3},
+# ]
+
+# boards = [
+#     {
+#         "id": 1,
+#         "title": "To visit"
+#     },
+#     {
+#         "id": 2,
+#         "title": "To do"
+#     },
+#     {
+#         "id": 3,
+#         "title": "To read"
+#     },
+# ]
+
+
+# @boards_bp.route("/<board_id>",methods=["GET"])
+# def get_board(board_id):
+#     board_id = int(board_id)
+#     for board in boards:
+#         if board_id == board["id"]:
+#             return ({
+#                 "id": board["id"],
+#                 "title" : board["title"]
+#             })
+
+# @boards_bp.route("/<board_id>",methods=["GET"])
+# def get_board(board_id):
+#     #retrieve specific board from database
+#     board = Board.query.get(board_id)
+#     for board in boards:
+#         if board_id == board["id"]:
+#             return ({
+#                 "id": board["id"],
+#                 "title" : board["title"]
+#             })
+
+# @boards_bp.route("/<board_id>/cards",methods=["GET"])
+# def get_board_cards(board_id):
+#     board_id = int(board_id)
+#     card_list = []
+#     for card in cards:
+#         if board_id == card["board_id"]:
+#             card_list.append(card)
+#         for board in boards:
+#             if board_id == board["id"]:
+#                 board_info = {
+#                     "id": board["id"],
+#                     "title" : board["title"],
+#                     "cards" : card_list
+#                 }
+#     return jsonify(board_info)
+
