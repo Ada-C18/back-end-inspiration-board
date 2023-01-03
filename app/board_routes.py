a8893436dac970b1697e48981047e1585de167f6
galinaramaniuk <galina.sergarz@gmail.com>
2023-01-04 00:14:45 +0300

    Created Board class. Added functions to routes

--- app/board_routes.py
+++ app/board_routes.py
@@ -1,19 +1,58 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
+from app.models.board import Board
 
 board_bp = Blueprint("board",__name__,url_prefix = "/boards")
 
+def get_validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
+
 #Read ALL boards
 @board_bp.route("/", strict_slashes=False, methods="GET")
+def read_all_boards():
+    boards = Board.query.all()
+    print(boards)
+    boards_list = [board.to_dict() for board in boards]
+    return make_response(jsonify(boards_list), 200)
 
 #Delete Board
-@board_bp.route("/", strict_slashes=False, methods="DELETE")
+@board_bp.route("/<board_id>", strict_slashes=False, methods="DELETE")
+def delete_board(board_id):
+    board = get_validate_model(Board, board_id)
+
+    db.session.delete(board)
+    db.session.commit()
+
+    response_body = {"message": f'Board "{board.name}": #{board_id} was deleted.'}
+    return make_response(jsonify(response_body), 200)
 
 #Update Board
 @board_bp.route("/<board_id>", strict_slashes=False, methods="PUT")
 
 #Create Board
 @board_bp.route("/", strict_slashes=False, methods="POST")
+def create_board():
+    try:
+        request_body = request.get_json()
+        new_board = Board.from_dict(request_body)
+
+    except:
+        return make_response({"message": "Invalid data"}, 400)
+
+    db.session.add(new_board)
+    db.session.commit()
+
+    return make_response(jsonify(new_board.to_dict()), 201)
 
 #Read ALL cards
 @board_bp.route("/<board_id>/cards", strict_slashes=False, methods="GET")
--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,20 @@
 from app import db
+
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    owner = db.Column(db.String, nullable=False)
+
+    def to_dict(self):
+        result_dict = {}
+        result_dict["board_id"] = self.board_id
+        result_dict["name"] = self.name
+        result_dict["owner"] = self.owner
+
+
+    def from_dict(cls, data_dict):
+        return cls(
+            name = data_dict["name"],
+            owner = data_dict["owner"]
+        )
