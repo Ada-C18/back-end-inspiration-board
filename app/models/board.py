6841f78091fe5c2b40ceed29620969cd6fe03da5
Billie Betts <lily.a.betts@gmail.com>
2023-01-03 10:45:09 -0500

    set-up

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,12 @@
 from app import db
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
+    # owner = db.relationship("Task", back_populates="goal")
+
+    def to_dict(self):
+            return {
+                "title": self.goal_id,
+                "owner": self.title
+            }
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,12 @@
 from app import db
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
+    # tasks = db.relationship("Task", back_populates="goal")
+
+    def to_dict(self):
+            return {
+                "message": self.message,
+                "likes_count": self.likes_count
+            }
