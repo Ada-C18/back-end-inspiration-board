66f55fced73c4485e55ba8f851f42109efe8c704
mhc <mhchau0+ada@gmail.com>
2023-01-03 16:15:14 -0500

    created board_routes &  card_routes & validate_model helper func

---
+++ app/board_routes.py
@@ -0,0 +1,17 @@
+from app import db
+from app.models.board import Board
+from app.models.card import Card
+from flask import Blueprint, request, jsonify, make_response, abort
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
