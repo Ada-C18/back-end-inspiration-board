3666b79dc9bb4e53c47da0b1e1a358b84c85e491
Paje <ejap99@gmail.com>
2023-01-03 13:43:59 -0600

    Post card and get cards added to routes

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,22 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.board import Board
+from app.models.card import Card
 
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
 
 @boards_bp.route("", methods=["POST"])
 def create_board():
@@ -14,3 +28,35 @@ def create_board():
     db.session.commit()
 
     return make_response(jsonify(f"Board {new_board.title} successfully created"), 201)
+
+@boards_bp.route("/<board_id>/cards", methods=["POST"])
+def create_card(board_id):
+
+    board = validate_model(Board, board_id)
+
+    request_body = request.get_json()
+    new_card = Card(
+        message=request_body["message"],
+        likes=request_body["likes"],
+        board=board
+    )
+    db.session.add(new_card)
+    db.session.commit()
+    return make_response(jsonify(f"Card {new_card.id} in {new_card.board.title} successfully created"), 201)
+
+@boards_bp.route("/<board_id>/cards", methods=["GET"])
+def retrieve_cards(board_id):
+
+    board = validate_model(Board, board_id)
+
+    cards_response = []
+    for card in board.cards:
+        cards_response.append(
+            {
+            "id": card.id,
+            "message": card.message,
+            "likes": card.likes
+            }
+        )
+    return jsonify(cards_response)
+
