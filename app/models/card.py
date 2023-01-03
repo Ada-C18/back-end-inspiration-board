f876278d647aeeda7548ddd4894456adf339cc38
Pavi <pavanirannulu@gmail.com>
2023-01-03 11:35:41 -0800

    added class methods

--- app/models/card.py
+++ app/models/card.py
@@ -6,3 +6,11 @@ class Card(db.Model):
     likes = db.Column(db.Integer)
     board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"), nullable=True)
     board = db.relationship("Board", back_populates="cards", lazy=True)
+
+    def from_instance_to_dict(self):
+        instance_dict = {"id":self.card_id, "message": self.message, "likes": self.likes}
+        return instance_dict
+
+    @classmethod
+    def from_dict_to_instance(cls, card_dict):
+        return cls(message=card_dict["message"], likes=card_dict["likes"])
