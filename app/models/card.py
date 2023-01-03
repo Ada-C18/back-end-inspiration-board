df279125a0e0f544a652e3cf8cb985e5cf8fb9ec
Eve Stroud <evestroud89@gmail.com>
2023-01-03 10:54:44 -0500

    Add Card model and register in app

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,9 @@
 from app import db
+
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"))
+    message = db.Column(db.String(140))
+    likes_count = db.Column(db.Integer)
+    board = db.relationship("Board", back_populates="cards")
