8cfb4399dc48cf9d0cba3111fb5d6638a4fbd6f2
Yangweiyi Li <lywy.amy@gmail.com>
2023-01-03 10:54:22 -0800

    add relationship of models

--- app/models/card.py
+++ app/models/card.py
@@ -4,3 +4,5 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+    board_id =  db.Column(db.Integer, db.ForeignKey('board.board_id'), default=None, nullable=True)
+    board=db.relationship('Board', back_populates='cards')
