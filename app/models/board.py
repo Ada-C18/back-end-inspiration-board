b5ebf46c23c26f943db178810c2900ed06dfae17
Annie <annie.gallagher7@gmail.com>
2023-01-03 12:46:33 -0700

    adds create route for board and to_dict & from_dict to board model

--- app/models/board.py
+++ app/models/board.py
@@ -1,6 +1,16 @@
 from app import db
 
+
 class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+
+    def to_dict(self):
+        return dict(id=self.board_id, title=self.title, owner=self.owner)
+
+    @classmethod
+    def from_dict(cls, board_data):
+        new_board = cls(title=board_data["title"], owner=board_data["owner"])
+
+        return new_board
--- app/routes/board.py
+++ app/routes/board.py
@@ -1,4 +1,20 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
 bp = Blueprint("board_bp", __name__, url_prefix="/board")
+
+
+@bp.route("", methods=["POST"])
+def create_board():
+    request_body = request.get_json()
+    new_board = Board.from_dict(request_body)
+
+    db.session.add(new_board)
+    db.session.commit()
+
+    board_dict = new_board.to_dict()
+
+    return make_response(jsonify({"board": board_dict}), 201)
+
+    # add a try/except later for missing data
