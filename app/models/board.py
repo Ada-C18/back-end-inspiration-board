093c6acd474a16ea4d99e342d76744bb6a3a0f7a
reneesu99 <reneesu22@gmail.com>
2023-01-03 10:40:52 -0800

    added restrictions to model attr

--- app/models/board.py
+++ app/models/board.py
@@ -3,6 +3,6 @@ from app import db
 
 class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String)
-    owner_name = db.Column(db.String)
+    title = db.Column(db.String, nullable=False)
+    owner_name = db.Column(db.String, nullable=False)
     cards = db.relationship("Card", back_populates="board")
