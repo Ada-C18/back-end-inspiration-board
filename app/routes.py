c83c7872f725ce5352cc71b09872a31a58630f62
Tasha <tashachang2@gmail.com>
2023-01-03 11:17:33 -0800

    Added Get All Boards endpoint, registered blueprint, created to_dict function in Board model.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,13 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
-# example_bp = Blueprint('example_bp', __name__)
+board_bp = Blueprint("board", __name__, url_prefix = "/board")
+
+@board_bp.route("", methods=["GET"])
+def get_all_boards():
+    boards = Board.query.all()
+
+    result = []
+    for item in boards:
+        result.append(item.to_dict())
