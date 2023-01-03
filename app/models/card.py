beb3fd6f51dc2fb556ec9d8661b0c896d86acdd8
Anaiq <>
2023-01-03 15:58:59 -0600

    completed Card to_dict and from_dict functions

--- app/models/card.py
+++ app/models/card.py
@@ -8,3 +8,19 @@ class Card(db.Model):
     board = db.relationship("Board", back_populates="cards")
     board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"))
 
+def to_dict(self):
+    card_as_dict = {}
+    card_as_dict["id"] = self.card_id
+    card_as_dict["message"] = self.message
+    card_as_dict["likes_count"] = self.likes_count
+    card_as_dict["board_id"] = self.board_id
+
+    return card_as_dict
+
+@classmethod
+def from_dict(cls, request_body):
+    return Card(
+        message=request_body["message"],
+        likes_count=request_body["likes_count"]
+    )
+    
