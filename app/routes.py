0ab1c56174e2910bfd5282a200846ea596f763fd
Samantha Hunt <ariaandsam@gmail.com>
2023-01-03 11:25:08 -0800

    Added comments for guidance -Sam

--- app/routes.py
+++ app/routes.py
@@ -2,3 +2,14 @@ from flask import Blueprint, request, jsonify, make_response
 from app import db
 
 # example_bp = Blueprint('example_bp', __name__)
+
+#Blueprints for Boards
+#Blueprints for Cards
+
+#Endpoints
+#GET - /boards
+#POST -/boards
+#GET - /boards/<board_id>/cards
+#POST - /boards/<board_id>/cards
+#DELETE - /cards/<card_id>
+#PUT - /cards/<card_id>/like
