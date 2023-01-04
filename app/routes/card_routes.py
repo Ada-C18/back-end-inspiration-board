d10dc0879d2a357ac4a3de79b4cfaf7a178c1754
takhat <takhat014@gmail.com>
2023-01-04 10:56:53 -0800

    added card routes

---
+++ app/routes/card_routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, jsonify, request, abort, make_response
+from app import db
+from app.models.board import Board
+from app.models.card import Card
+from .routes_helper import get_one_obj_or_abort
+
+card_bp = Blueprint("card",__name__, url_prefix="/card")
+@card_bp.route("", methods=['POST'])
+def add_card():
+    request_body = request.get_json()
+    new_card = Card.from_dict(request_body)
+
+    db.session.add(new_card)
+    db.session.commit()
+    
+    return {
+        "message": f"Successfully created new card with id: {new_card.card_id}"
+    }, 201
+
+    
+
