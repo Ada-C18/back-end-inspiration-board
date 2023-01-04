138abb428ea3d64d6aa33e55628781e20a70a385
Annie <annie.gallagher7@gmail.com>
2023-01-04 12:06:01 -0700

    updates to and from dict func to include board_id info

--- app/models/card.py
+++ app/models/card.py
@@ -9,12 +9,19 @@ class Card(db.Model):
     board = db.relationship("Board", back_populates="cards")
 
     def to_dict(self):
-        return dict(id=self.card_id, message=self.message, likes_count=self.likes_count)
+        return dict(
+            id=self.card_id,
+            message=self.message,
+            likes_count=self.likes_count,
+            board_id=self.board_id,
+        )
 
     @classmethod
     def from_dict(cls, card_data):
         new_card = cls(
-            message=card_data["message"], likes_count=card_data["likes_count"]
+            message=card_data["message"],
+            likes_count=card_data["likes_count"],
+            board_id=card_data["board_id"],
         )
 
         return new_card
