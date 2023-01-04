797406927d344bf67df6c33372080071959f067d
takhat <takhat014@gmail.com>
2023-01-04 10:58:22 -0800

    added to_dict and from_dict methods in card model

--- app/models/card.py
+++ app/models/card.py
@@ -4,3 +4,14 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+    
+    def to_dict(self):
+        return {
+            "card_id": self.card_id,
+            "message": self.message,
+            "likes_count": self.likes_count,
+        }
+    @classmethod
+    def from_dict(cls, cls_dict):
+        return cls(message = cls_dict["message"])
+
