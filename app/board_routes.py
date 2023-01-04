37579138d0d45616494247deda79b989a2c234d4
n2020h <uzame.va@gmail.com>
2023-01-03 21:04:21 -0500

    add delete_card and get_all_boards

--- app/board_routes.py
+++ app/board_routes.py
@@ -5,13 +5,34 @@ from app.validate_data import validate_model
 
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 
-
-#===================================
+# ===================================
 #        READ ONE BOARD BY ID
-#===================================
+# ===================================
+
 
 @boards_bp.route("/<board_id>", methods=["GET"])
 def read_one_board(board_id):
     board = validate_model(Board, board_id)
     response_one_board = {"board": Board.to_dict(board)}
     return jsonify(response_one_board), 200
+
+
+# ===================================
+#        READ ALL BOARDS
+# ===================================
+@boards_bp.route("", methods=["GET"])
+def read_all_boards():
+
+    boards = Board.query.all()
+
+    boards_response = []  # returns empty list if no tasks
+
+    for board in boards:
+        boards_response.append(
+            {
+                "id": board.board_id,
+                "title": board.title,
+                "owner": board.owner,
+            }
+        )
+    return jsonify(boards_response), 200
--- app/card_routes.py
+++ app/card_routes.py
@@ -1,9 +1,26 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 from app.models.card import Card
 from app.validate_data import validate_model
 
+# may need to validate board id first
 
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 
+# ===================================
+#        DELETE ONE CARD BY ID
+# ===================================
 
+
+@cards_bp.route("/<card_id>", methods=["DELETE"])
+def delete_card(card_id):
+    card = validate_model(Card, card_id)
+
+    # update database?
+    # db.session.delete(card)
+    # db.session.commit()
+
+    # return delete message to user
+    delete_message = f'Card {card_id} successfully deleted'
+    return delete_message, 200
