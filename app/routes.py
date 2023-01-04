8a999d307eaee0953e259a257e1197f453b8b8ec
Kate <varindakate@gmail.com>
2023-01-04 10:20:47 -0800

    added get_one_object_or_abort helper function; driver: kate, nav: kate

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.board import Board
 from app.models.card import Card
@@ -7,6 +7,25 @@ from app.models.card import Card
 # example_bp = Blueprint('example_bp', __name__)
 boards_bp = Blueprint("boards_bp", __name__, url_prefix="/boards")
 cards_bp = Blueprint("cards_bp", __name__, url_prefix="/cards")
+
+def get_one_object_or_abort(cls, object_id):
+    try:
+        object_id = int(object_id)
+    except ValueError:
+        response_str = f"Invalid ID: {object_id}"
+        abort(make_response(jsonify({
+            "message": response_str
+            }), 400))
+
+    matching_object = cls.query.get(object_id)
+
+    if not matching_object:
+        response_str = f"{object_id} not found"
+        abort(make_response(jsonify({
+            "message": response_str
+            }), 404))
+    
+    return matching_object
 
 #Endpoints
 #GET - /boards
@@ -36,9 +55,11 @@ def create_board():
 
     return make_response(f"{new_board.title} created", 201)
 
-
-
 #GET - /boards/<board_id>/cards
+
+
+
+
 #POST - /boards/<board_id>/cards
 #DELETE - /cards/<card_id>
 #PUT - /cards/<card_id>/like
