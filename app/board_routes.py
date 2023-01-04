cf1bad3a0d5777ab9510c48deca47d0b130b2d6b
Alia Athman <aliaathmann@gmail.com>
2023-01-04 13:03:16 -0500

    creates the add_card_to_board function in board_routes to link the cards to boards

--- app/board_routes.py
+++ app/board_routes.py
@@ -36,20 +36,20 @@ def read_all_cards(board_id):
 
 
 @board_bp.route("/<board_id>/cards", methods=["POST"])
-def create_card(board_id):
+def add_card_to_board(board_id):
     request_body = request.get_json()
     if len(request_body) != 1:
         return {"details": "Invalid Data"}, 400
-    new_card = Card.from_dict_to_object(request_body)
+    # new_card = Card.from_dict_to_object(request_body)
 
     board = validate_model(Board,board_id)
-    board = board.to_dict(cards=True)
-    board.cards.append(new_card)
+    card_id = request_body["card_id"]
+    # board = board.to_dict(cards=True)
+    board.cards.append(Card.query.get(card_id))
     
-    db.session.add(new_card)
     db.session.commit()
 
-    return make_response(jsonify({{'board_id': board.board_id, 'messages': [card.message for card in board['cards']]}}))
+    return make_response(jsonify({'board_id': board.board_id, 'card': [card.message for card in board.cards]}), 200)
 
     # return make_response(jsonify({"card": new_card.to_dict()}), 201)
 
