bf1a3afb92d3afa82671c476cc02a7a1a61c45f0
Minh <minh.seikel@gmail.com>
2023-01-04 10:42:54 -0800

    Alyssa/Minh - established relationship between Card and Board models, update Board model to add to_dict() method to render columns in dictionary format

--- app/models/board.py
+++ app/models/board.py
@@ -2,6 +2,15 @@ from app import db
 
 
 class Board(db.Model):
-    board_id = db.Column(db.Integer, primary_key=True)
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
-    owner = db.Column(db.String)    
+    owner = db.Column(db.String)
+    cards = db.relationship('Card', back_populates='board', lazy=True)    
+    
+    def to_dict(self):
+        dict = {
+            "id": self.board_id,
+            "title": self.title,
+            "owner": self.owner
+            
+        }
--- app/models/card.py
+++ app/models/card.py
@@ -4,5 +4,7 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+    board = db.relationship('Card', back_populates='cards')
     
     
