bbdc48146aefa832e940dcda3a755266d5f065b2
Nashwa <n.alsharki@gmail.com>
2023-01-04 09:37:03 -0800

    Initial setup completed

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,7 @@
 from app import db
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    description = db.Column(db.String, nullable = False)
+    like_count = db.Column(db.Integer)
+    board = db.relationship('Board', back_populates="card", lazy = True)
--- app/routes.py
+++ app/routes.py
@@ -2,3 +2,6 @@ from flask import Blueprint, request, jsonify, make_response
 from app import db
 
 # example_bp = Blueprint('example_bp', __name__)
+
+boards_bp = Blueprint('boards_bp',__name__, url_prefix ='/boards')
+
