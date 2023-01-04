5f5d80525009fa4b3b67116d36afb7464534aabd
Uche Audrey <uaudrey3@gmail.com>
2023-01-04 14:08:19 -0500

    get cards and post card routes

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.board import Board
+from app.models.card import Card
 
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint("board_bp", __name__, url_prefix="/boards")
@@ -35,3 +36,42 @@ def get_all_boards():
             "owner": board.owner
         })
     return jsonify(boards_response), 200
+
+
+@board_bp.route("/<board_id>/cards", methods=["GET"])
+def get_cards_from_board(board_id):
+    board = validate_model(Board, board_id)
+
+    cards = []
+    for card in board.cards:
+        cards.append({
+            "id": card.card_id,
+            "message": card.message,
+            "likes": card.likes_count
+        })
+
+    return jsonify(cards), 200
+
+
+@board_bp.route("<board_id>", methods=["POST"])
+def add_card_to_board(board_id):
+    board = validate_model(Board, board_id)
+    request_body = request.get_json()
+
+    if "message" not in request_body:
+        return {
+            "details": "Invalid data"
+        }, 400
+
+    new_card = Card(
+        message=request_body["message"]
+    )
+
+    board.cards.append(new_card)
+
+    db.session.add(new_card)
+    db.session.commit()
+
+    return {
+        "New card successfully created"
+    }, 200
