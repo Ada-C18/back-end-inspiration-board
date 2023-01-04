03643c09625b11b0d45847b550257d36f07a6f51
n2020h <uzame.va@gmail.com>
2023-01-04 14:00:26 -0500

    "resolve merges"

--- app/models/board.py
+++ app/models/board.py
@@ -1,28 +1,21 @@
 from app import db
 
+
 class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
-<<<<<<< HEAD
-
 
     def to_dict(self):
         return {
             "id": self.board_id,
             "title": self.title,
-            "owner": self.owner 
+            "owner": self.owner
         }
 
-
-=======
-    
-    
-    
     @classmethod
     def from_dict(cls, req_body):
         return cls(
             title=req_body["title"],
             owner=req_body["owner"]
         )
->>>>>>> 017f7b4cf16f59f84eeac4b99eacfe570b1a59be
