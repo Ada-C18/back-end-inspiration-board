fe6b40e5ec8a21c8685a3a77ad7a3a2c1bf7aa32
Eve Stroud <evestroud89@gmail.com>
2023-01-04 13:50:31 -0500

    Add another test for boards/<board_id>/cards

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -44,6 +44,19 @@ def test_get_all_boards_one_board_three_cards(client, one_board_three_cards):
     ]
 
 
+def test_get_all_cards_one_board_no_cards(client, one_board):
+    # Arrange
+    board_id = Board.query.first().board_id
+
+    # Act
+    response = client.get(f"/boards/{board_id}/cards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+
 def test_get_all_cards_one_board_three_cards(client, one_board_three_cards):
     # Arrange
     board_id = Board.query.first().board_id
