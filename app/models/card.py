b07b5323b52e9c2a272dd89dc77572042ba3037c
Eve Stroud <evestroud89@gmail.com>
2023-01-04 10:21:40 -0500

    Add default of 0 to Card.likes_count

--- app/models/card.py
+++ app/models/card.py
@@ -5,5 +5,5 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"))
     message = db.Column(db.String(140))
-    likes_count = db.Column(db.Integer)
+    likes_count = db.Column(db.Integer, default=0)
     board = db.relationship("Board", back_populates="cards")
