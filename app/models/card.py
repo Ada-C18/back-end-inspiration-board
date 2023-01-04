7b415aa684fef08be73d4a6254df9d07369c406d
bukunmig <bgesinde44@icloud.com>
2023-01-04 15:01:30 -0800

    Update: refactored card model

--- app/models/card.py
+++ app/models/card.py
@@ -4,22 +4,25 @@ from datetime import datetime
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date_created = db.Column(db.DateTime)
-    card_title = db.Column(db.String)
-    visible = db.Column(db.Boolean)
-    card_owner = db.Column(db.String, db.ForeignKey('user.id'))
-    user = db.relationship("User", back_populates="boards")
+    owner_id = db.Column(db.String, db.ForeignKey('user.id'))
+    owner = db.relationship("Owner", back_populates="cards")
+    likes = db.Column(db.Integer)
+    message = db.Column(db.String)
 
     def to_dict(self):
         card_as_dict = {}
         card_as_dict["id"] = self.id
         card_as_dict["date_created"] = self.date_created
-        card_as_dict["card_title"] = self.card_title
-        card_as_dict["visible"] = self.visible
+        card_as_dict["owner_id"] = self.owner_id
+        card_as_dict["owner"] = self.owner
+        card_as_dict["likes"] = self.likes
+        card_as_dict["message"] = self.message
+        
 
         return card_as_dict
 
     @classmethod
     def from_dict(cls, card_data):
-        new_card = Card(date_created=card_data["date_created"], card_title=card_data["card_title}]"], card_owner=card_data["card_owner"], visible=card_data["visible"])
+        new_card = Card(date_created=card_data["date_created"], owner=card_data["owner"], likes=card_data["likes"], message=card_data["message"])
 
         return new_card
