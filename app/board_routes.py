2c973782d7ae4453057eb540f1de98f8dcb079f2
Thao Le <thaole0325@gmail.com>
2023-01-04 11:46:37 -0500

    made POST endoint for 1 board

--- app/board_routes.py
+++ app/board_routes.py
@@ -3,3 +3,19 @@ from app import db
 from app.models.board import Board
 
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+#======================================
+#        CREATE ONE BOARD        
+#====================================== use from_dict class method 
+@boards_bp.route("", methods=["POST"])
+def create_board():
+    request_body = request.get_json()
+    if "title" not in request_body or \
+    "description" not in request_body:
+        return make_response("Invalid Request", 400)
+    
+    new_board = Board.from_dict(request_body)
+    
+    db.session.add(new_board)
+    db.session.commit()
+
+    return jsonify({"board": new_board.to_dict()}), 201
--- app/models/board.py
+++ app/models/board.py
@@ -4,3 +4,12 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+    
+    
+    
+    @classmethod
+    def from_dict(cls, req_body):
+        return cls(
+            title=req_body["title"],
+            owner=req_body["owner"]
+        )
