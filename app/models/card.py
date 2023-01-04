3f3a425ff48f8bcfed316ad7bac0bf51a772a81e
Tasha <tashachang2@gmail.com>
2023-01-04 11:25:49 -0800

    added board relationship in Card model

--- app/models/card.py
+++ app/models/card.py
@@ -4,7 +4,10 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
-    
+    board = db.relationship("Board", back_populates = "cards")
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+
+
     def to_dict(self):
         return {
             "card_id": self.card_id,
