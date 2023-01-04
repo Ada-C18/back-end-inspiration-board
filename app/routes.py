360ca874a1de149c9dd0b87042d4d74a3dd267a9
Alyssa Reyes <reyesalyssa18@gmail.com>
2023-01-04 11:08:10 -0800

    Minh and Alyssa create GET route for one board

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import abort, Blueprint, request, jsonify, make_response
 from app import db
 from app.models.board import Board
 from app.models.card import Card
@@ -16,3 +16,10 @@ def get_all_boards():
         result.append(board.to_dict())
         # need to create to_dict method in Board Model
     return jsonify(result), 200
+
+#GET route for ONE board
+def get_board_from_id(board_id):
+    chosen_board = Board.query.get(board_id)
+    if chosen_board is None:
+        return abort(make_response({"msg": f"Could not find board with id: {board_id}"}, 404))
+    return chosen_board
