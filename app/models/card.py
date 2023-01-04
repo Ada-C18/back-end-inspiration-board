3eeb1b5f94f8445172cab7fd04a0e076d2279466
fataneh <fkarandish1@gmail.com>
2023-01-03 16:05:56 -0800

    add to_dict and from_dict to card model

--- app/models/card.py
+++ app/models/card.py
@@ -4,3 +4,17 @@ class Card(db.Model):
     card_id= db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count =db.Column(db.Integer)
+
+    def to_dict(self):
+        card_dict={
+            "id":self.card_id,
+            "message":self.message,
+            "likes_count":self.likes_count
+        }
+        return card_dict
+
+    @classmethod
+    def from_dict(cls,data_dict):
+        new_obj =cls(
+            message= data_dict["message"])
+        return new_obj
