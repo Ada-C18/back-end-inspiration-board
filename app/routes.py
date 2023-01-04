40d8262e652078d157aec419010f5b46e0c753bc
reneesu99 <reneesu22@gmail.com>
2023-01-04 09:52:34 -0800

    added get

--- app/routes.py
+++ app/routes.py
@@ -14,3 +14,19 @@ def create_board():
     db.session.commit()
 
     return make_response(jsonify(f"Board {new_board.title} successfully created"), 201)
+
+@boards_bp.route("", methods=["GET"])
+def read_all_boards():
+    
+    boards = Board.query.all()
+
+    boards_response = []
+    for board in boards:
+        boards_response.append(
+            {
+                "title": board.title,
+                "owner_name": board.owner_name
+
+            }
+        )
+    return jsonify(boards_response)
