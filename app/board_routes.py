217e2a11f4dc8ad60754c94f319a75627aba9f00
Presley <persleytieman@outlook.com>
2023-01-04 12:54:08 -0500

    Finished get routes for Board & tests

--- app/board_routes.py
+++ app/board_routes.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.board import Board
 # example_bp = Blueprint('example_bp', __name__)
 
 board_bp = Blueprint('boards', __name__, url_prefix="/boards")
+
+def validate_model(cls, model_id):
+    try:
+        model_id=int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__}{model_id} invalid"}, 400))
+    model =  cls.query.get(model_id)
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} was not found"}, 404))
+    return model
 
 #Get Routes
 @board_bp.route("", methods = ['GET'])
@@ -14,3 +24,10 @@ def get_all_boards():
     all_boards = [board.to_dict() for board in boards]
 
     return jsonify(all_boards)
+
+@board_bp.route("/<board_id>", methods=["GET"])
+def get_one_board(board_id):
+    board = validate_model(Board, board_id)
+    return {"board": board.to_dict()}, 200
+
+
--- tests/test_board_routes.py
+++ tests/test_board_routes.py
@@ -23,3 +23,18 @@ def test_get_boards_one_saved_board(client, one_board):
         'title': 'Get Healthy',
         'owner': 'Isabella'
     }]
+
+def test_get_specific_board(client, one_board):
+
+    response = client.get("/boards/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert "board" in response_body
+    assert response_body == {
+        "board" : {
+            "id": 1,
+            "title": "Get Healthy",
+            "owner": "Isabella"
+        }
+    }
