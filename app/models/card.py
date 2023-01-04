ce22f4530953bc1a47c81399465432bbfa2b2480
Andrea-Galindo <andreagalindo.guerra@gmail.com>
2023-01-04 10:05:44 -0500

    modified board and card models

--- app/models/card.py
+++ app/models/card.py
@@ -4,5 +4,7 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     message = db.Column(db.String, nullable=False)
     likes_count = db.Column(db.Integer)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=False)
+    board = db.relationship("Board", back_populates='cards')
 
 
