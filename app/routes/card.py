e6394bde68ce10a369bfd290063e591d7069cf67
Cristal <cbcodee@gmail.com>
2023-01-04 11:59:54 -0800

    added delete card

--- app/routes/card.py
+++ app/routes/card.py
@@ -1,8 +1,21 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.card import Card
 
 bp = Blueprint("card_bp", __name__, url_prefix="/card")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
 
 
 @bp.route("", methods=["POST"])
@@ -17,5 +30,11 @@ def create_card():
 
     return make_response(jsonify({"card": card_dict}), 201)
 
-# @bp.route("", methods=["GET"])
-# def read_cards():
+@bp.route("<card_id>", methods=["DELETE"])
+def delete_card(card_id):
+    card = validate_model(Card, card_id)
+
+    db.session.delete(card)
+    db.session.commit()
+
+    return {"details": f'Card {card.card_id} successfully deleted'}
