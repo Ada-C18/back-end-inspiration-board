48296ec38fd58b2e0830a834cfe7a6df94360ae3
Rahel Kemal <rahelskemal@gmail.com>
2023-01-04 12:44:49 -0800

    fixed model bug

--- app/models/board.py
+++ app/models/board.py
@@ -4,4 +4,4 @@ class Board(db.Model):
     board_id = db.Column(db.Integer , primary_key=True)
     author = db.Column(db.String)
     title= db.Column(db.String)
-    messages = db.relationship("Card", back_populates="board") 
+    cards = db.relationship("Card", back_populates="board") 
