dda73a27452a7757035c4daf2e19e0f1d161d195
Eve Stroud <evestroud89@gmail.com>
2023-01-04 09:51:35 -0500

    Finish testing boilerplate

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,19 @@
 import pytest
 from app import create_app
+from app.models.board import Board
+from app.models.card import Card
 from app import db
+from flask.signals import request_finished
 
 
 @pytest.fixture
 def app():
     # create the app with a test config dictionary
     app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
 
     with app.app_context():
         db.create_all()
