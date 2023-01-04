649fb96baa66f2f69997f98c310ba03565bad245
Eve Stroud <evestroud89@gmail.com>
2023-01-04 13:39:14 -0500

    Add test for GET /boards/<board_id>/cards

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -42,3 +42,22 @@ def test_get_all_boards_one_board_three_cards(client, one_board_three_cards):
             "owner": "Test Owner",
         }
     ]
+
+
+def test_get_all_cards_one_board_three_cards(client, one_board_three_cards):
+    # Arrange
+    board_id = Board.query.first().board_id
+
+    # Act
+    response = client.get(f"/boards/{board_id}/cards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [
+        {
+            {board_id: board.board_id, message: "Hello", likes_count: 0},
+            {board_id: board.board_id, message: "Test", likes_count: 1},
+            {board_id: board.board_id, message: "Goodbye", likes_count: 2},
+        }
+    ]
