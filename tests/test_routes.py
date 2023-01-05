3624a727834646c187df1843dcf113bdeeca1b64
Cristal <cbcodee@gmail.com>
2023-01-05 10:48:18 -0800

    working on CRUD tests as additional feature

---
+++ tests/test_routes.py
@@ -0,0 +1,27 @@
+import pytest
+from app.models.card import Card
+from app.models.board import Board
+
+
+def test_create_board(client): 
+    #Act
+    response = client.post("/board", json={
+        "title": "Inpirational Quotes",
+        "owner": "Cristal",
+    })
+
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert "board" in response_body
+    assert response_body == {
+        "board": {
+            "id": 1,
+            "title": "Inspirational Quotes",
+            "owner": "Cristal",
+                    }
+    }
+    # new_board = Board.query.get(1)
+    # assert new_board
+    # asser new_board.
