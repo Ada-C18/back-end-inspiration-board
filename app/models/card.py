c39a9bea0b242298812a77daa7e4994b2afc4fdf
Rahel Kemal <rahelskemal@gmail.com>
2023-01-04 19:40:54 -0800

    minor changes

--- app/models/card.py
+++ app/models/card.py
@@ -4,5 +4,5 @@ from app import db
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
-    board_id = db.Column(db.Integer, db.ForeignKey("board.id"))
+    board_id = db.Column(db.Integer, db.ForeignKey("board.id"), nullable=True)
     board = db.relationship("Board", back_populates="cards")
