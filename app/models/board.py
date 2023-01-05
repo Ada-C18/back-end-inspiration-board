8de08041abedbfbe99e1dccce57984cf4a7a496d
hang tran <hangmtran101@gmail.com>
2023-01-05 11:07:20 -0800

    relationship between models created

--- app/models/board.py
+++ app/models/board.py
@@ -5,6 +5,7 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+    cards = db.relationship("Card", back_populates = "board", lazy = True)
 
 
     def to_dict(self):
@@ -23,3 +24,5 @@ class Board(db.Model):
             owner = data_dict["owner"])
 
             return new_obj
+
+    
