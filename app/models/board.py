de1241f7b87810ac4f03b4d6de16d1271e1e6d04
kimiahasani <kn.improve@gmail.com>
2023-01-05 11:20:16 -0800

    create a delete and patch routes , worked with intu

--- app/models/board.py
+++ app/models/board.py
@@ -3,7 +3,7 @@ class Board (db.Model):
     board_id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
-    # cards = db.relationship("card", )
+    cards = db.relationship("Card", back_populates = "board", lazy = True)
 
     def to_dict(self):
         return {
--- app/models/card.py
+++ app/models/card.py
@@ -4,12 +4,14 @@ class  Card(db.Model):
     card_id = db.Column(db.Integer, primary_key = True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer, default = 0)
+    board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"), nullable = True)
+    board = db.relationship("Board",back_populates = "cars")
 
     def to_dict(self):
         card_dict =  {
             'id': self.card_id,
             'message': self.message,
-            'likes_count': self.likes_count,
+            'is_like': True if self.likes_count else False,
         }
         if self.board_id:
             card_dict["board_id"] = self.board_id
