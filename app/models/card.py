b6a846fb0892d26cd8dbfd9ad811c2864452f76c
Isabella Van Stedum <isabella.vanstedum@gmail.com>
2023-01-04 20:06:52 -0600

    made likes_count in Cards Model default to 0

--- app/models/card.py
+++ app/models/card.py
@@ -4,7 +4,7 @@ from app import db
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
-    likes_count = db.Column(db.Integer)
+    likes_count = db.Column(db.Integer, default=0)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
     board = db.relationship("Board", back_populates="cards")
 
@@ -12,12 +12,14 @@ class Card(db.Model):
         return dict(
             id = self.card_id,
             message = self.message,
+            likes_count =self.likes_count
         )
 
     @classmethod
     def from_dict(cls, card_data):
         the_card = cls(
             message = card_data["message"]
+        
         )
 
         return the_card
