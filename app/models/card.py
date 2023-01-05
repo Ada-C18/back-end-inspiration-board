d767dbb8242cc44f1ee0c5ff2ce87a0cf03303ed
Tasha <tashachang2@gmail.com>
2023-01-05 11:06:41 -0800

    refactored from_dict in card model to catch errors for missing information. commented out add_card route in card_routes.py because this is aleady in board_routes.py

--- app/models/card.py
+++ app/models/card.py
@@ -1,4 +1,5 @@
 from app import db
+from flask import Blueprint, request, jsonify, make_response, abort
 
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -7,14 +8,32 @@ class Card(db.Model):
     board = db.relationship("Board", back_populates = "cards")
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
 
-
     def to_dict(self):
         return {
             "card_id": self.card_id,
             "message": self.message,
             "likes_count": self.likes_count,
         }
+
     @classmethod
     def from_dict(cls, cls_dict):
-        return cls(message = cls_dict["message"])
+        if "message" in cls_dict and "likes_count" in cls_dict:
+            validate_message_is_not_empty_string(cls_dict)
+
+            return cls(message = cls_dict["message"], likes_count = cls_dict["likes_count"])
+
+        elif "message" in cls_dict:
+            validate_message_is_not_empty_string(cls_dict)
+
+            return cls(message = cls_dict["message"])
+        else:
+            response_str = "missing necessary information"
+            abort(make_response(jsonify({"message":response_str}), 404))
+
+
+def validate_message_is_not_empty_string(dict):
+    if dict["message"] == "":
+        response_str = "please enter a message"
+        abort(make_response(jsonify({"message":response_str}), 404))
+
 
--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -5,26 +5,26 @@ from app.models.card import Card
 from .routes_helper import get_one_obj_or_abort
 
 card_bp = Blueprint("card",__name__, url_prefix="/card")
-@card_bp.route("", methods=['POST'])
-def add_card():
-    request_body = request.get_json()
-    new_card = Card.from_dict(request_body)
+# @card_bp.route("", methods=['POST'])
+# def add_card():
+#     request_body = request.get_json()
+#     new_card = Card.from_dict(request_body)
 
-    db.session.add(new_card)
-    db.session.commit()
+#     db.session.add(new_card)
+#     db.session.commit()
 
-    return {
-        "message": f"Successfully created new card with id: {new_card.card_id}"
-    }, 201
+#     return {
+#         "message": f"Successfully created new card with id: {new_card.card_id}"
+#     }, 201
 
 @card_bp.route("/<card_id>", methods=["DELETE"])
 def delete_one_card(card_id):
     chosen_card = get_one_obj_or_abort(Card, card_id)
 
     db.session.delete(chosen_card)
-    
-    db.session.commit() 
-    
+
+    db.session.commit()
+
     return jsonify({
         "message": f"Successfully deleted card with id: {card_id}"
     }), 200
