36b7318dc20ad9837a519485b105ca8faf313b48
kimiahasani <kn.improve@gmail.com>
2023-01-05 10:38:37 -0800

    migrate & upgrade

--- app/card_routes.py
+++ app/card_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response, abort, os
+from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.card import Card
 
