674da48bee295e6b665b63fb1d8777472eea39da
Yael <yaelechols@gmail.com>
2023-01-05 14:33:01 -0800

    Implements one to many relationship with Card in Board model

--- app/models/board.py
+++ app/models/board.py
@@ -5,7 +5,7 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
-
+    cards = db.relationship("Card", back_populates="board", lazy=True)
 
     @classmethod
     def from_dict(cls, req_body):
@@ -13,8 +13,6 @@ class Board(db.Model):
             title=req_body["title"],
             owner=req_body["owner"]
         )
-
-
 
     def to_dict(self):
         return {
