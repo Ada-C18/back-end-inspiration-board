9f023f0e55eb618cd6c84667499396ee32b29884
Melissa <ma.pena94@gmail.com>
2023-01-05 11:13:51 -0800

    Creating relationships card/board

--- app/models/board.py
+++ app/models/board.py
@@ -4,13 +4,18 @@ class Board(db.Model):
     board_id=db.Column(db.Integer, primary_key=True, autoincrement=True )
     title = db.Column(db.String)
     owner=db.Column(db.String)
+    cards=db.relationship("Card", back_populates="board")
 
     def to_dict(self):
-        return {
+        board= {
             "board_id":self.board_id,
             "title": self.title,
             "owner":self.owner
         }
+        if self.cards:
+            board["cards"]= [card.to_dict() for card in self.cards]
+
+        return board 
 
     def from_dict(board_data):
         return Board (
--- app/models/card.py
+++ app/models/card.py
@@ -4,16 +4,22 @@ class Card(db.Model):
     card_id=db.Column(db.Integer, primary_key=True, autoincrement=True )
     message = db.Column(db.String)
     likes_count=db.Column(db.Integer)
+    board_id= db.Column(db.Integer, db.ForeignKey('board.board_id'), default=None)
+    board= db.relationship("Board", back_populates="cards")
 
     def add_likes(self):
         self.likes_count += 1
 
     def to_dict(self):
-        return {
+        card= {
             "card_id": self.card_id,
             "message": self.message,
             "likes_count": self.likes_count
         }
+        if self.board:
+            card["board_id"] = self.board.board_id
+
+        return card
 
     def from_dict(card_data):
         return Card (
