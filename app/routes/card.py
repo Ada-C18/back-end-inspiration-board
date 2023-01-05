3af829114175abfddaddbe5db62ae80b80fe148f
Tara <talsaidi@ucdavis.edu>
2023-01-05 11:38:14 -0800

    updated add_like_to_card endpoint for Card

--- app/routes/card.py
+++ app/routes/card.py
@@ -12,13 +12,13 @@ def validate_card_id(card_id):
     except ValueError:
         abort(make_response(jsonify({"message": "card_id must be an integer"}),400))
     
-    matching_task = Card.query.get(card_id)
+    matching_card = Card.query.get(card_id)
 
-    if matching_task is None:
+    if matching_card is None:
         response_str = f"Card with id {card_id} was not found in the database."
         abort(make_response(jsonify({"message": response_str}), 404))
 
-    return matching_task
+    return matching_card
 
 
 
@@ -54,13 +54,15 @@ def get_all_cards():
     return jsonify(response), 200
 
 
-# WEHER DO WE STORE LIKE DATA? LOCAL STATE? API CALL EVERY TIME?
-# @card_bp.route("/<card_id>", methods=["PUT"])
-# def add_like_to_card(card_id):
-#     card = validate_card_id(card_id)
-#     card_dict = card.to_dict()
+# WHERE DO WE STORE LIKE DATA? LOCAL STATE? API CALL EVERY TIME?
+@card_bp.route("/<card_id>", methods=["PUT"])
+def add_like_to_card(card_id):
+    card = validate_card_id(card_id)
+
+    card.likes_count += 1
+    card_dict = card.to_dict()
     
-#     return jsonify({"card":card_dict})
+    return jsonify({"card":card_dict})
 
 
 
