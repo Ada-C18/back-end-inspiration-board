c385d6ec98a64ef1a3ac42d8959f6782ca7c9625
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2023-01-05 11:47:45 -0800

    Comment extra routes for card

--- app/routes/cards.py
+++ app/routes/cards.py
@@ -8,25 +8,25 @@ import os
 # example_bp = Blueprint('example_bp', __name__)
 cards_bp = Blueprint('cards',  __name__ , url_prefix='/cards') 
 
-@cards_bp.route('', methods=['POST'])
-def create_one_card():
-    request_body = request.get_json()
-    try:
-        new_card = Card(message=request_body['message'])
-    except KeyError:
-        return jsonify({"details": "Invalid data"}), 400
-    db.session.add(new_card)
-    db.session.commit()
-    return jsonify(new_card.to_dict()), 201
+# @cards_bp.route('', methods=['POST'])
+# def create_one_card():
+#     request_body = request.get_json()
+#     try:
+#         new_card = Card(message=request_body['message'])
+#     except KeyError:
+#         return jsonify({"details": "Invalid data"}), 400
+#     db.session.add(new_card)
+#     db.session.commit()
+#     return jsonify(new_card.to_dict()), 201
     
 
-@cards_bp.route('', methods=['GET'])
-def get_all_cards():
-    card_response = []
-    cards = Card.query.all()  
-    for card in cards:
-        card_response.append(card.to_dict()) 
-    return jsonify(card_response), 200
+# @cards_bp.route('', methods=['GET'])
+# def get_all_cards():
+#     card_response = []
+#     cards = Card.query.all()  
+#     for card in cards:
+#         card_response.append(card.to_dict()) 
+#     return jsonify(card_response), 200
 
 
 @cards_bp.route('/<card_id>', methods=['DELETE'])
