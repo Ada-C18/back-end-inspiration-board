b0aec3af6df65ec2de59f0986cd0d9284e947bbd
Silvia <sx0771@gmail.com>
2023-01-05 09:50:38 -0500

    completed create a board and get all boards

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,32 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
+from app.models.board import card
 
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint('board_bp', __name__, url_prefix='/board')
 card_bp = Blueprint('card_bp', __name__, url_prefix='/card')
+
+@board_bp.route("", methods=["GET", "POST"])
+def handle_boards():
+    if request.method == "GET":
+        boards = Board.query.all()
+        boards_response = []
+        for board in boards:
+            boards_response.append({
+                "board_id": board.board_id,
+                "title": board.title,
+                "owner": board.owner,
+            })
+        return jsonify(boards_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        title = request_body.get("title")
+        owner = request_body.get("owner")
+        new_board = Board(title=request_body["title"], 
+                        owner=request_body["owner"])
+
+        db.session.add(new_board)
+        db.session.commit()
+    
+    return make_response(f"Board {new_board.title} successfuly create", 201)
