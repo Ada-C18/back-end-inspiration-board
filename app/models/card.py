f2b88a3565f002187a94c21ac4eee52d79cff55f
Marilyn Thai <marilynthai15@gmail.com>
2023-01-05 11:27:20 -0800

    Backend routes revisions

--- app/models/card.py
+++ app/models/card.py
@@ -3,13 +3,13 @@ from app import db
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
-    likes_count = db.Column(db.Integer)
+    likes_count = db.Column(db.Integer,default = 0)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
 
     def to_dict(self):
         if self.card_id:
             return {
                 "id":self.card_id,
-                "message":self.card.message,
-                "likes":self.card.likes_count
+                "message":self.message,
+                "likes":self.likes_count
             }
