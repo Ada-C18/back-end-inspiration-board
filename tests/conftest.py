b651c1859aef83dbf13e89822991e37f4b99e9bd
Niloofar Nayebi <nil_nyb@yahoo.com>
2023-01-06 14:22:51 -0500

    working on test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 from app import create_app
+from app.models.card import Card
+from app.models.board import Board
 from app import db
-
+from flask.signals import request_finished
 
 @pytest.fixture
 def app():
@@ -20,3 +22,13 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+# This fixture gets called in every test that
+# references "one_card"
+# This fixture creates a card and saves it in the database
+@pytest.fixture
+def one_card(app):
+    new_card = Card(
+        message="Go on my daily walk")
+    db.session.add(new_card)
+    db.session.commit()
