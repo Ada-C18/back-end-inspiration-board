dd0af157fa65acdb8ae32759914bfb9843e1e2c2
Carina <checarina@gmail.com>
2023-01-06 11:43:24 -0800

    created GET and POST routes for cards

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -1,4 +1,39 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.card import Card
 
 # example_bp = Blueprint('example_bp', __name__)
+cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
+
+# GET all cards
+@cards_bp.route("", methods = ["GET"])
+def get_all_cards():
+    cards = Card.query.all()
+    cards_response = []
+    for card in cards:
+        cards_response.append(
+            {
+              "card_id": card.card_id,
+              "message": card.message,
+              "likes": card.likes_count  
+            }
+        )
+    return jsonify(cards_response)
+
+# POST create new card
+@cards_bp.route("", methods = ["POST"])
+def create_card():
+    request_body = request.get_json()
+    new_card = Card(
+        message = request_body["message"],
+        likes_count = 0
+    )
+
+    db.session.add(new_card)
+    db.session.commit()
+    
+    return make_response(jsonify({"card": new_card.message}), 201)
+
+# UPDATE heart count on a card
+
+#Delete
