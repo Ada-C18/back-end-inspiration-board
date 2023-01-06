6e1ae9440974626d2f6eaa96d9b2f51ebbea8c33
Annie <annie.gallagher7@gmail.com>
2023-01-06 11:45:40 -0700

    updates one_board pytest fixture to add board and then separately cards. fixes read one board test to pass

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app import db
 from app.models.board import Board
+from app.models.card import Card
 
 
 @pytest.fixture
@@ -22,62 +23,46 @@ def app():
 def client(app):
     return app.test_client()
 
+
 @pytest.fixture
 def three_boards(app):
-    db.session.add_all([
-        Board(
-            title="Inspirational Quotes",
-            owner="Cristal",
-            # cards=[],
-        ),
-        Board(
-            title="To-do",
-            owner="Annie",
-            # cards=[],
-        ),
-        Board(
-            title="Thoughts",
-            owner="Cristal",
-            # cards=[],
-        ),
-    ])
+    db.session.add_all(
+        [
+            Board(
+                title="Inspirational Quotes",
+                owner="Cristal",
+                # cards=[],
+            ),
+            Board(
+                title="To-do",
+                owner="Annie",
+                # cards=[],
+            ),
+            Board(
+                title="Thoughts",
+                owner="Cristal",
+                # cards=[],
+            ),
+        ]
+    )
     db.session.commit()
+
 
 @pytest.fixture
 def one_board(app):
     new_board = Board(
         title="Inspiration",
         owner="Cristal",
-        cards=[{
-                "board_id": 1,
-                "id": 1,
-                "likes_count": 54,
-                "message": "does this return an int"
-            },
-            {
-                "board_id": 1,
-                "id": 2,
-                "likes_count": 0,
-                "message": "we're testing more"
-            },
-            {
-                "board_id": 1,
-                "id": 3,
-                "likes_count": 17,
-                "message": "lots of cards"
-            },
-            {
-                "board_id": 1,
-                "id": 4,
-                "likes_count": 20,
-                "message": "no sql databases"
-            },
-            {
-                "board_id": 1,
-                "id": 5,
-                "likes_count": 6,
-                "message": "no concept of migrations"
-            }],
     )
     db.session.add(new_board)
+    db.session.commit()
+    db.session.add_all(
+        [
+            Card(board_id=1, likes_count=54, message="does this return an int"),
+            Card(board_id=1, likes_count=0, message="we're testing more"),
+            Card(board_id=1, likes_count=17, message="lots of cards"),
+            Card(board_id=1, likes_count=20, message="no sql databases"),
+            Card(board_id=1, likes_count=6, message="no concept of migrations"),
+        ]
+    )
     db.session.commit()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -53,7 +53,7 @@ def test_read_all_boards(client, three_boards):
     # Act
     response = client.get("/board")
     response_body = response.get_json()
-    
+
     # Assert
     assert response.status_code == 200
     assert len(response_body) == 3
@@ -78,47 +78,47 @@ def test_read_all_boards(client, three_boards):
         },
     ]
 
+
 def test_read_one_board(client, one_board):
     # Act
     response = client.get("/board/Inspiration")
     response_body = response.get_json()
-    
+
     # Assert
     assert response.status_code == 200
     assert len(response_body) == 1
     assert response_body == {
-        "board": {"title": "Inspiration",
-        "owner": "Cristal",
-        "cards":[{
-                "board_id": 1,
-                "id": 1,
-                "likes_count": 54,
-                "message": "does this return an int"
-            },
-            {
-                "board_id": 1,
-                "id": 2,
-                "likes_count": 0,
-                "message": "we're testing more"
-            },
-            {
-                "board_id": 1,
-                "id": 3,
-                "likes_count": 17,
-                "message": "lots of cards"
-            },
-            {
-                "board_id": 1,
-                "id": 4,
-                "likes_count": 20,
-                "message": "no sql databases"
-            },
-            {
-                "board_id": 1,
-                "id": 5,
-                "likes_count": 6,
-                "message": "no concept of migrations"
-            }],
+        "board": {
+            "id": 1,
+            "title": "Inspiration",
+            "owner": "Cristal",
+            "cards": [
+                {
+                    "board_id": 1,
+                    "id": 1,
+                    "likes_count": 54,
+                    "message": "does this return an int",
+                },
+                {
+                    "board_id": 1,
+                    "id": 2,
+                    "likes_count": 0,
+                    "message": "we're testing more",
+                },
+                {"board_id": 1, "id": 3, "likes_count": 17, "message": "lots of cards"},
+                {
+                    "board_id": 1,
+                    "id": 4,
+                    "likes_count": 20,
+                    "message": "no sql databases",
+                },
+                {
+                    "board_id": 1,
+                    "id": 5,
+                    "likes_count": 6,
+                    "message": "no concept of migrations",
+                },
+            ],
         }
     }
 
