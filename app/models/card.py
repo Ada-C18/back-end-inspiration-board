f3fa5a3c0bf3088bdd21e26bad94af366bae3fd0
Yael <yaelechols@gmail.com>
2023-01-05 16:57:30 -0800

    Implemenets to_dict and from_dict methods in Card model

--- app/models/card.py
+++ app/models/card.py
@@ -6,3 +6,17 @@ class Card(db.Model):
     likes_count = db.Column(db.Integer)
     board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"))
     board = db.relationship("Board", back_populates="cards")
+
+    def to_dict(self):
+        card = {
+            "id": self.card_id,
+            "message": self.message,
+            "likes_count": self.likes_count,
+            "board_id": self.board_id
+        }
+
+        return card
+
+    @classmethod
+    def from_dict(cls, card_data, board):
+        return cls(message=card_data["message"], likes_count=card_data["likes_count"], board_id=board.board_id)
