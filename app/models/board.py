8148f1f73482c6f1a13bb387d4b84ae56f0f5348
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2023-01-06 13:33:48 -0500

    Fixed routes

--- app/models/board.py
+++ app/models/board.py
@@ -5,6 +5,8 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     owner = db.Column(db.String, nullable=False)
+    cards = db.relationship("Card", back_populates="board", lazy= True)
+
 
     def to_dict(self):
         result_dict = {}
