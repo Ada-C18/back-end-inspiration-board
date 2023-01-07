e84a1d5c4f701e109e6ed254f7e5019da1125f68
Dominique <dominiquedaba@gmail.com>
2023-01-06 17:55:51 -0800

    Tasha refactored update_likes function to get info from request body

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -29,16 +29,27 @@ def delete_one_card(card_id):
         "message": f"Successfully deleted card with id: {card_id}"
     }), 200
 
-@card_bp.route("/<card_id>/<inc_or_dec>", methods=["PATCH"])
-def update_likes_for_one_card(card_id, inc_or_dec):
+# @card_bp.route("/<card_id>/<inc_or_dec>", methods=["PATCH"])
+# def update_likes_for_one_card(card_id, inc_or_dec):
+#     chosen_card = get_one_obj_or_abort(Card, card_id)
+#     if chosen_card.likes_count is None:
+#         chosen_card.likes_count = 0
+#     if inc_or_dec == "inc":   
+#         chosen_card.likes_count+=1
+#     if inc_or_dec == "dec" and chosen_card.likes_count > 0:
+#         chosen_card.likes_count-=1
+
+#     db.session.commit()
+#     return jsonify({"message": f"Successfully updated the likes count for Card ID {card_id}"}), 200
+
+@card_bp.route("/<card_id>", methods=["PATCH"])
+def update_likes_for_one_card(card_id):
     chosen_card = get_one_obj_or_abort(Card, card_id)
-    if chosen_card.likes_count is None:
-        chosen_card.likes_count = 0
-    if inc_or_dec == "inc":   
-        chosen_card.likes_count+=1
-    if inc_or_dec == "dec" and chosen_card.likes_count > 0:
-        chosen_card.likes_count-=1
+    request_body= request.get_json()
+
+    chosen_card.likes_count = request_body["likes_count"]
 
     db.session.commit()
+
     return jsonify({"message": f"Successfully updated the likes count for Card ID {card_id}"}), 200
 
