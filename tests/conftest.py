01714725607ee99e7ca9ccf0d73f18d4b15fbdb5
Rahel Kemal <rahelskemal@gmail.com>
2023-01-13 16:30:41 -0800

    added all routes tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,9 +26,43 @@ def client(app):
 # This fixture gets called in every test that
 # references "one_card"
 # This fixture creates a card and saves it in the database
+
 @pytest.fixture
 def one_card(app):
     new_card = Card(
-        message="Go on my daily walk")
+        message="You are doing great!")
     db.session.add(new_card)
     db.session.commit()
+
+@pytest.fixture
+def three_cards(app):
+    db.session.add_all([
+        Card(
+            message="You are powerful"
+        ),
+        Card(
+            message="Today is going to be a good day"
+        ),
+        Card(
+            message="The stars want you to be successful"
+        )
+    ])   
+    
+    db.session.commit()
+
+@pytest.fixture
+def one_board(app):
+
+    new_board = Board(title="January Board", author="r")
+    db.session.add(new_board)
+    db.session.commit()
+
+@pytest.fixture
+def one_card_belongs_to_one_board(app, one_board, one_card):
+    card = Card.query.first()
+    board = Board.query.first()
+    board.cards.append(card)
+    db.session.commit()
+
+
+
