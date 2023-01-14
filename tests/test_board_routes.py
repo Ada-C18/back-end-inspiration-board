236dbafbd7289ee73027da0836fd4fcd2c6fdf38
Tasha <tashachang2@gmail.com>
2023-01-14 11:04:39 -0800

    Created fixtures in conftest and added test_board_routes.py and test_card_routes.py. Running into an error when trying to run test_board_routes.py

---
+++ tests/test_board_routes.py
@@ -0,0 +1,7 @@
+
+# def test_get_all_boards_with_empty_db_returns_empty_list(client):
+#     response = client.get("/board")
+#     response_body = response.get_json()
+
+#     assert response.status_code == 200
+#     assert response_body == []
