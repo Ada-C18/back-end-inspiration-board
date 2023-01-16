9e85bca12b2b2d0274552c134f2515cc1f8db3c0
Anaiq <>
2023-01-15 20:20:35 -0600

    added fixtures to conftest.py one and four boards, one card, created test_model file, created tests for no,one,four boards

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from app import create_app
+from app.models.board import Board
+from app.models.card import Card
 from app import db
-from flask.signals import request_finished
 
 
 @pytest.fixture
@@ -9,10 +10,6 @@ def app():
     # create the app with a test config dictionary
     app = create_app({"TESTING": True})
 
-    @request_finished.connect_via(app)
-    def expire_session(sender, response, **extra):
-        db.session.remove()
-        
     with app.app_context():
         db.create_all()
         yield app
@@ -25,3 +22,31 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+# This fixture creates a task and saves it in the database
+@pytest.fixture
+def one_board(app):
+    new_board = Board(
+        title="test board 1",
+        owner="QP/Lin"
+    )
+    db.session.add(new_board)
+    db.session.commit()
+
+#  This fixture creates four tasks and saves them in the database
+@pytest.fixture
+def four_boards(app):
+    db.session.add_all([
+        Board(title="test board 1", owner="QP/Lin"),
+        Board(title="test board 2", owner="QP/Lin"),
+        Board(title="test board 3", owner="QP/Lin"),
+        Board(title="test board 4", owner="QP/Lin"),
+    ])
+    db.session.commit()
+
+# This fixture creates a goal and saves it in the database
+@pytest.fixture
+def one_card(app):
+    new_card = Card(message="test to get the third card", likes_count=0)
+    db.session.add(new_card)
+    db.session.commit()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,3 +4,74 @@ from app.models.board import Board
 from app.models.card import Card
 import pytest
 
+def test_get_all_books_no_saved_boards(client):
+    # Act
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+
+def test_get_all_books_one_saved_boards(client, one_board):
+    # Act
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 1
+    assert response_body == [
+        {
+            "id": 1,
+            "title": "test board 1",
+            "owner": "QP/Lin"
+        }
+    ]
+
+
+def test_get_all_books_four_saved_boards(client, four_boards):
+    # Act
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 4
+    assert response_body == [
+        {
+            "id": 1,
+            "title": "test board 1",
+            "owner": "QP/Lin"
+        },
+        {
+            "id": 2,
+            "title": "test board 2",
+            "owner": "QP/Lin"
+        },
+        {
+            "id": 3,
+            "title": "test board 3",
+            "owner": "QP/Lin"
+        },
+        {
+            "id": 4,
+            "title": "test board 4",
+            "owner": "QP/Lin"
+        }
+    ]
+
+def test_get_specific_board(client, four_boards):
+    # Act
+    response = client.get("boards/3")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert "title" in response_body
+    assert response_body == {
+        "id": 3,
+        "title": "test board 3",
+        "owner": "QP/Lin"
+    }
