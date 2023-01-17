93ac4db2982653ca8bf9104d172daf4e6edfe5b5
bukunmig <bgesinde44@icloud.com>
2023-01-17 14:19:52 -0800

    updated owner to author

--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     boards = db.relationship("Board", back_populates="owner")
-    cards = db.relationship("Card", back_populates="owner")
+    cards = db.relationship("Card", back_populates="author")
 
 # only show the delete button in the frontend itself if the owner of the board matches the currently logged in user
