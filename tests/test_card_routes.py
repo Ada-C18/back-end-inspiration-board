e4d6e90a6382794b5f0cda4890a39434576d26e6
Yael <yaelechols@gmail.com>
2023-01-16 19:28:27 -0800

    Implements tests for DELETE one card

--- tests/test_card_routes.py
+++ tests/test_card_routes.py
@@ -1,7 +1,12 @@
 from werkzeug.exceptions import HTTPException
 from app.models.card import Card
+from app.models.board import Board
 import pytest
 
 
-def test_delete_one_card(client):
-    pass
+def test_delete_one_card(client, one_card_to_one_board):
+    response = client.delete("/cards/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert Board.query.get(1).cards == []
