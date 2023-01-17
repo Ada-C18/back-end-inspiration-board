cf79cab25316681891c2aabe8877083d6384ec59
Yael <yaelechols@gmail.com>
2023-01-17 08:53:15 -0800

    Adds test cases for delete card

--- tests/test_card_routes.py
+++ tests/test_card_routes.py
@@ -10,3 +10,18 @@ def test_delete_one_card(client, one_card_to_one_board):
 
     assert response.status_code == 200
     assert Board.query.get(1).cards == []
+
+def test_delete_card_not_found(client):
+    response = client.delete("/cards/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {"message": "Card 1 not found"}
+
+def test_delete_card_invalid_id(client):
+    response = client.delete("/cards/xxx")
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {"message": "Card xxx has an invalid id"}
+
