34d363eb2ca130d667183083123aa2c4434b192c
Emily <sonemily+github@gmail.com>
2023-01-18 17:01:33 -0500

    Update card_color column in PSQL.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,19 @@
 import pytest
 from app import create_app
 from app import db
+from flask.signals import request_finished
+from app.models.user import User
+from app.models.board import Board
 
 
 @pytest.fixture
 def app():
     # create the app with a test config dictionary
     app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
 
     with app.app_context():
         db.create_all()
@@ -20,3 +27,18 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def saved_user(app):
+    test_user = User(name="Test")
+
+    db.session.add_all()
+    db.session.commit()
+
+@pytest.fixture
+def two_saved_boards(app):
+    hacker_board = Board(title="Hackspiration Board", owner_id = 1)
+    underwater_clown_board = Board(title="Underwater Clown Board", owner_id = 1)
+
+    db.session.add_all()
+    db.session.commit()
---
+++ tests/test_routes.py
@@ -0,0 +1,5 @@
+from werkzeug.exceptions import HTTPException
+from app.board_routes import validate_model
+from app.models.board import Board
+import pytest
+
