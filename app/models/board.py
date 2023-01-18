e7a95176314c4591b3411eac6ba60580e80f5869
Tiffany Tran <tif.tran86@gmail.com>
2023-01-18 14:10:44 -0800

    update Board.py to return cards in response object

--- app/models/board.py
+++ app/models/board.py
@@ -1,4 +1,6 @@
 from app import db
+from flask import jsonify
+
 # from app.models.card import Card
 
 #ONE to many relationship
@@ -10,11 +12,24 @@ class Board(db.Model):
 
 
     def to_dict(self):
+        
+        cards_list = []
+        for card in self.cards:
+            cards_list.append(card.to_dict())
+
         return {
             "title": self.title,
             "owner": self.owner,
-            "id": self.board_id
+            "id": self.board_id,
+            "cards": cards_list
         }
+        
+
+
+
+        
+        
+            
 
 
     @classmethod
