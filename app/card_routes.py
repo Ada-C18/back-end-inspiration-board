20800d116b58e8e656e6b561eceba6e497a3c71b
Katherine Abrikian <kabrikian@gmail.com>
2023-01-17 21:13:00 -0500

    route outlines for Card

--- app/card_routes.py
+++ app/card_routes.py
@@ -2,3 +2,12 @@ from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 
 bp = Blueprint("cards_bp", __name__, url_prefix="/cards")
+
+# Create a card [POST]
+
+# Get information on a card by ID [GET]
+# Get information on all cards associated with a board [GET]
+
+# Update information on a card [PATCH]
+
+# Delete card from database / board [DELETE]
