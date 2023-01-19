7a173fde8f5f5b7e1c6a4bdb34524c25c436242a
takhat <takhat014@gmail.com>
2023-01-19 08:58:00 -0800

    refactored card model to set default likes_count to 0, and resfactored corresponding tests

--- tests/test_board_routes.py
+++ tests/test_board_routes.py
@@ -84,13 +84,19 @@ def test_delete_board_not_found(client):
 
 def test_post_one_card_belonging_to_a_board(client, one_board):
     response = client.post("board/1/card", json={
-        "message": "It's a great day!"
+        "message": "It's a great day!", 
     })
+    
     response_body = response.get_json()
 
     assert response.status_code == 201
     assert response_body == {
-        "message": "Card It's a great day! belonging to Inspirational Quotes successfully added"
+        "message": "Card It's a great day! belonging to Inspirational Quotes successfully added",
+        "card": {
+            "card_id": 1,
+            "likes_count": 0,
+            "message": "It's a great day!"
+        }
     }
 
 def test_get_all_cards_belonging_to_one_board_one_card(client, one_card_belongs_to_one_board):
