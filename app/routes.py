201d4cf8c6ff92b63cc11b834e5beca4aed1b40a
Rahel Kemal <rahelskemal@gmail.com>
2023-01-20 12:52:54 -0800

    code cleanup

--- app/routes.py
+++ app/routes.py
@@ -7,29 +7,6 @@ from app.models.board import Board
 
 cards_bp = Blueprint("cards",__name__, url_prefix="/cards")
 
-# @cards_bp.route("", methods=["POST", "GET"])
-# def get_or_post_cards():
-#     if request.method=="GET":
-#         cards=Card.query.all()
-#         cards_response=[]
-#         for card in cards:
-#             cards_response.append({
-#                 "id":card.id,
-#                 "message":card.message
-#             })
-#         return jsonify(cards_response)
-    
-#     elif request.method =="POST" :
-#         request_body=request.get_json()
-#         new_card = Card(message= request_body["message"])
-
-#         db.session.add(new_card)
-#         db.session.commit()
-
-#         return make_response(f"Card {new_card.message} successfully created", 201)
-
-
-
 
 def validate_id(cls, id):
     try: 
