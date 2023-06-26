bf7dd6a0fb1f56e27d116c9c661166c22924ce74
Dee Williams <deejmwilliams@gmail.com>
2023-06-26 13:42:39 -0400

    Add comments describing endpoints

--- app/board_routes.py
+++ app/board_routes.py
@@ -2,3 +2,11 @@ from flask import Blueprint, request, jsonify, make_response
 from app import db
 
 # example_bp = Blueprint('example_bp', __name__)
+
+#Read board (GET)
+
+#Read all boards? (GET)
+
+#Create board (POST)
+
+#Delete board (DELETE)
---
+++ app/card_routes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, request, jsonify, make_response
+from app import db
+
+# example_bp = Blueprint('example_bp', __name__)
+
+#Read all cards from board (GET)
+
+#Add card to board (POST)
+
+#Delete card from board (DELETE)
