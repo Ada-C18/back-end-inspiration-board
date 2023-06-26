db05c94888a82d06eb67da4db640a4b9fa826774
Allie Soliz <solizale@gmail.com>
2023-06-26 15:28:46 -0400

    adds GET and DELETE endpoints

--- app/board_routes.py
+++ app/board_routes.py
@@ -1,27 +1,56 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 from app.models.board import Board
+from app.helpers import validate_model
 
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint("boards", __name__, url_prefix="/boards")
 
-#Create board (POST)
-@board_bp.route("", methods=["POST"])
-def create_new_board():
+
+@board_bp.route("/boards", methods=["POST"])
+def create_board():
     request_body = request.get_json()
 
     new_board = Board(title=request_body["title"],
-                    owner=request_body["owner"])
+                      owner=request_body["owner"])
 
     db.session.add(new_board)
     db.session.commit()
 
     return make_response(f"Board {new_board.title} successfully created", 201)
 
-#Read board (GET)
 
-#Read all boards? (GET)
+@board_bp.route("", methods=["GET"])
+def get_boards():
+    boards_response = []
+    for board in boards:
+        boards_response.append(
+            {
+                "id": board.id,
+                "title": board.title,
+                "owner": board.owner
+            }
+        )
+    return jsonify(boards_response)
 
 
+@board_bp.route("/<board_id>", methods=["GET"])
+def get_one_board(board_id):
+    board_id = int(board_id)
+    for board in boards:
+        if board.id == board_id:
+            return {
+                "id": board.id,
+                "title": board.title,
+                "owner": board.owner,
+            }
 
-#Delete board (DELETE)
+
+@board_bp.route("/<board_id>", methods=["DELETE"])
+def delete_board(board_id):
+    board = validate_model(Board, board_id)
+
+    db.session.delete(board)
+    db.session.commit()
+
+    return make_response(jsonify(f"Board #{board.id} successfully deleted"))
