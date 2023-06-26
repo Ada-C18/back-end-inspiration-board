3e6b578eb9b79b6853e60f29a2241b53bdaaf106
Allie Soliz <solizale@gmail.com>
2023-06-26 14:11:20 -0400

    sets up blueprint board_bp

--- app/board_routes.py
+++ app/board_routes.py
@@ -2,11 +2,14 @@ from flask import Blueprint, request, jsonify, make_response
 from app import db
 
 # example_bp = Blueprint('example_bp', __name__)
+board_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+#Create board (POST)
 
 #Read board (GET)
 
 #Read all boards? (GET)
 
-#Create board (POST)
+
 
 #Delete board (DELETE)
